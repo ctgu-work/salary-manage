3b6fd5723db35f42392d8410a3759f80af55555a
wh <1090230661@qq.com>
2020-08-05 13:29:35 +0800

    更新配置

--- src/main/java/com/ctgu/salary/SalaryApplication.java
+++ src/main/java/com/ctgu/salary/SalaryApplication.java
@@ -3,9 +3,11 @@ package com.ctgu.salary;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 //@MapperScan("com.ctgu.salary.dao")
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SalaryApplication {
 
 	public static void main(String[] args) {
